ad09e12a978e6f0f1d4703a094e5f4e5ecda2fde
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2023-03-17 15:08:55 -0300

    change-in-server

--- backend/server.js
+++ backend/server.js
@@ -32,15 +32,15 @@ app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/users", userRouter);
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
-
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5500;
 app.listen(`${port}`, () => {
